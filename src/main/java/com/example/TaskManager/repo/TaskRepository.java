b001059415c0980f0157867d618ce255e4ab35ae
detskiyded <dodustester@gmail.com>
2026-03-03 17:07:54 +0400

    feat: add task repository for h2 connection

---
+++ src/main/java/com/example/TaskManager/repo/TaskRepository.java
@@ -0,0 +1,13 @@
+package com.example.TaskManager.repo;
+
+import com.example.TaskManager.models.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends CrudRepository<Task, Long> {
+
+}
